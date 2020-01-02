330cfef9e410328df3c399cd8e8e34252ed643bd
Andreas Roither <andi.roither@protonmail.com>
2020-01-02 15:11:31 +0100

    Update gitignore

---
+++ RiseUp/Assets/Scripts/Menu/MainMenu/MainMenuManager.cs
@@ -0,0 +1,62 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu.MainMenu
+{
+    /// <summary>
+    /// Manages Menu state and transition
+    /// </summary>
+    /// Author: Andreas Roither
+    public class MainMenuManager : MonoBehaviour
+    {
+        #region Fields
+
+        [Header("Menu Fields")] public GameObject mainMenu;
+        public GameObject optionsMenu;
+
+        // Text inputs
+        [Space] [Header("Text Fields")]
+        public TMP_InputField playerNameInputText;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Load next scene in the build index
+        /// </summary>
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        /// <summary>
+        /// Close application
+        /// </summary>
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+        
+        /// <summary>
+        /// Transition to Options Menu
+        /// </summary>
+        public void SwitchToOptionsMenu()
+        {
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+        }
+
+        /// <summary>
+        /// Transition to Main Menu
+        /// </summary>
+        public void SwitchToMainMenu()
+        {
+            optionsMenu.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+        
+        #endregion
+    }
+}
